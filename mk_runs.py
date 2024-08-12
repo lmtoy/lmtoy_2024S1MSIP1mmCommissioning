b14ada5acdc6a0eb6b52d2bff5474ba1851def66
Peter Teuben <teuben@gmail.com>
2024-08-12 02:23:11 +0000

    updz

--- mk_runs.py
+++ mk_runs.py
@@ -10,16 +10,16 @@ project="2024S1MSIP1mmCommissioning"
 
 #        obsnums per source (make it negative if not added to the final combination)
 on = {}
-on['foo'] = [ ]
+on['mars'] = [ 116442 ]
 
 
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
-pars1['foo']   = ""
+pars1['mars']   = ""
 
 #        common parameters per source on subsequent runs (run1b, run2b)
 pars2 = {}
-pars2['foo']   = "srdp=1 admit=0"
+pars2['mars']   = "srdp=1 admit=0"
 
 if __name__ == '__main__':    
-    runs.mk_runs(project, on, pars1, pars2, sys.argv)
+    runs.mk_runs(project, on, pars1, pars2, None, sys.argv)
